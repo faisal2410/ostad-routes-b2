2538aca7ef06ccdc827269ca49f8560ffd5814e0
Faisal ahmed <faisal2410@yahoo.com>
2023-02-01 21:59:51 +0600

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <div className="header">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/countries">Countries</Link>
+        </div>
+    );
+};
+
+export default Header;
