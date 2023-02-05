779c59d12140532d679781a1166d040bd2d76a6a
Faisal ahmed <faisal2410@yahoo.com>
2023-02-05 21:55:06 +0600

    add to cart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LoadCountries from "./components/LoadCountries";
 import NotFound from "./components/NotFound";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Cosmetics from './components/Cosmetics';
 
 
 function App() {
@@ -19,7 +20,8 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/products" element={<Products></Products>}></Route>       
-        <Route path="/countries" element={<LoadCountries></LoadCountries>}></Route>      
+        <Route path="/countries" element={<LoadCountries></LoadCountries>}></Route>  
+        <Route path="/cosmetics" element ={<Cosmetics></Cosmetics>}></Route>
         <Route path="*" element ={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
